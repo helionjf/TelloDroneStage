43aac277ebf4fc88701235eb93f870ca00df0dd3
Lohan Delomenie <lohan.delomenie@epitech.eu>
2021-04-08 09:57:04 +0200

    [MAJ] MAJ requirement.txt and coment error line in swarm test

--- test/SwarmTest.py
+++ test/SwarmTest.py
@@ -10,14 +10,14 @@ swarm.connect()
 swarm.takeoff()
 swarm.streamon()
 
-while True:
-    img = {}
-    for tello in swarm:
-        img = tello.get_frame_read().frame
-        for i in img:
-            img = cv2.resize(img, (360, 240))
-            cv2.imshow("Test", img)
-            cv2.waitKey(1)
+# while True:
+#  img = {}
+# for tello in swarm:
+#    img = tello.get_frame_read().frame
+#   for i in img:
+#       img = cv2.resize(img, (360, 240))
+#       cv2.imshow("Test", img)
+#       cv2.waitKey(1)
 
 swarm.move_up(100)
 
